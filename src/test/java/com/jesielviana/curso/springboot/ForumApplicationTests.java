1fa889618f1c99f3ce25c128ffb7a73b1dfc17a7
jesielviana <jesielviana@pm.me>
2022-07-22 12:14:22 -0300

    change java version

--- src/test/java/com/jesielviana/curso/springboot/ForumApplicationTests.java
+++ src/test/java/com/jesielviana/curso/springboot/ForumApplicationTests.java
@@ -3,8 +3,8 @@ package com.jesielviana.curso.springboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringbootApplicationTests {
+@SpringBootTest(classes = com.jesielviana.forum.ForumApplication.class)
+class ForumApplicationTests {
 
 	@Test
 	void contextLoads() {
